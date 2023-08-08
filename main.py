4cd7bacb16d31670e90effac082d19304b2e514f
patyfernandes86 <111815919+patyfernandes86@users.noreply.github.com>
2023-08-07 21:59:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+import random
+
+bd_produtos = []
+
+def adicionarProduto(codigo,produto, preco):
+    prod = {
+        "codigo": codigo,
+        "produto": produto,
+        "preco": preco
+    }
+    bd_produtos.append(prod)
+    print('produto cadastrado  com sucesso!')
+
+def editarProduto(codigo,produto,preco):
+    for prod in bd_produtos:
+        if prod["codigo"] == codigo:
+            prod["produto"] == produto
+            prod["preco"] == preco
+            print("Dados alterados com sucesso!")
+
+def pesquisarProduto(codigo):
+    encontrado = False
+    for produto in bd_produtos:
+        if produto["codigo"] == codigo:
+            print(f"Codigo: {produto['codigo']}")
+            print(f"Produto: {produto['produto']}")
+            print(f"Preco: {produto['preco']}")
+            print('---------------------')
+            encontrado = True
+
+    if not encontrado:
+        print('Produto não encontrado! ')
+
+
+def deletarProduto(codigo):
+    encontrado = False
+    for produto in bd_produtos:
+        if produto['codigo'] == codigo:
+            bd_produtos.remove(produto)
+            print("Produtos deletado com sucesso!")
+            encontrado = True
+
+
+
+
+
+
+while True:
+    print("------Bem vindo ao Menu---------")
+    opcao = int(input("1 - Adicionar produto \n"
+                      "2- -Editar produto \n"
+                      "3 - Pesquisar produto \n"
+                      "4 - Deletar produto \n"
+                      "5 - Listar produtos \n"
+                      "6 - Sair \n"
+                      "Selecione uma opção: "))
+
+    if opcao== 1:
+        cod = random.randint(1,1000)
+        produto = input('Digite o nome do produto:')
+        preco = float(input('Digite o preço do produto'))
+        adicionarProduto(cod,produto,preco)
+
+
+    elif opcao == 2:
+        cod = int(input('Digite o código do produto:'))
+        produto = input('Digite o nome do produto:')
+        preco == float(input("Digite o preço do produto:"))
+        editarProduto(cod,produto,preco)
+
+    elif opcao == 3:
+        cod = int(input('Digite o codigo do produto:'))
+        pesquisarProduto(cod)
+
+    elif opcao == 4:
+        cod = int(input('Digite o codigo do produto:'))
+        deletarProduto(cod)
+
+
+
+
+    elif opcao == 5:
+       print(bd_produtos)
+
+
+    elif opcao == 6:
+        break
